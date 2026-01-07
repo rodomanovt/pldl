fd7d47044c7019535e3b06318ea671db67ffce5b
timofey <timondeseptikon@gmail.com>
2026-01-07 15:27:29 +0300

    Optimize file system scan

--- src/pldl/core/MusicRepository.py
+++ src/pldl/core/MusicRepository.py
@@ -46,21 +46,21 @@ class MusicRepository:
         if not os.path.exists(work_dir):
             return []
 
-        mp3_files = [
+        mp3_names = [
             f for f in os.listdir(work_dir)
             if Path(f).suffix.lower() == ".mp3"
         ]
         
         if paths:
-            return [os.path.join(work_dir, f) for f in mp3_files]
+            return [os.path.join(work_dir, f) for f in mp3_names]
         else:
-            return mp3_files
-
+            return mp3_names
+        
 
     @staticmethod
-    def is_downloaded(playlist: Playlist, url: str, paths: list[str]) -> bool:
-        """checks url in file metadata to see if it was downloaded"""
-        url = url.replace("www.", "").replace("music.", "")
+    def get_all_downloaded_urls(paths: list[str]) -> set[str]:
+        """Extracts urls from files' metadata"""
+        result = set()
 
         for path in paths:
             if os.path.isfile(path):
@@ -71,27 +71,33 @@ class MusicRepository:
                 except Exception as e:
                     print(e)
 
-                
                 for frame in frames:
                     if frame.desc == "purl" and frame.text:
-                        current_url = str(frame.text[0]).strip().replace("www.", "").replace("music.", "")
-                        if current_url == url:
-                            return True
+                        url = str(frame.text[0]).strip().replace("www.", "").replace("music.", "")
+                        result.add(url)
 
             else:
                 print(f"File {path} does not exist")
-            
-        return False
+
+        return result
+
+
+    @staticmethod
+    def is_downloaded(target_url: str, downloaded_urls: list[str]) -> bool:
+        target_url = target_url.replace("www.", "").replace("music.", "")
+
+        return target_url in downloaded_urls
     
 
     @staticmethod
     def get_songs_to_download(playlist: Playlist) -> list[RemoteSong]: # raises Exeption
         all_remote_songs = MusicRepository.get_remote_songs_from_playlist(playlist)
-        all_local_songs = MusicRepository.get_local_song_names_from_playlist(playlist, paths=True)
+        all_local_song_paths = MusicRepository.get_local_song_names_from_playlist(playlist, paths=True)
+        all_downloaded_urls = MusicRepository.get_all_downloaded_urls(all_local_song_paths)
 
         result = []
         for song in all_remote_songs:
-            if not MusicRepository.is_downloaded(playlist, song.url, all_local_songs):
+            if not MusicRepository.is_downloaded(song.url, all_downloaded_urls):
                 result.append(song)
 
         return result
